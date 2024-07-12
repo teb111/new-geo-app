36169271ed6f84fc985e5441ad4f9369e8597d61
Oyewande <oyewandeoyediran@gmail.com>
2024-07-13 00:04:16 +0100

    adjusted the dashboard design

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import Signup from "./signup/Signup";
 import Login from "./login/Login";
 import Register from "./register/Register";
-// import forgotPassword from "./forgotPassword/forgotPassword";
 import BusinessDashbaord from "./dashboard/dashboard";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/reset" element={<forgotPassword />} />
       <Route path="/business-dashboard" element={<BusinessDashbaord />} />
     </Routes>
   );
